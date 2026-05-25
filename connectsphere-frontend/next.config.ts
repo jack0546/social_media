38d8f5751b072c854d515c1af9551b52a66de363
Jack Luis <jack@example.com>
2026-05-25 03:26:28 +0000

    Configure Next.js and GitHub Actions for GitHub Pages deployment

--- connectsphere-frontend/next.config.ts
+++ connectsphere-frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isGithubActions ? "/social_media" : "",
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "firebasestorage.googleapis.com" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
